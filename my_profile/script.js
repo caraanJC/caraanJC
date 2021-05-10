b96194e772a468b04ba0b2130119710c48da7dc9
creation00 <caraanjohncarlo@gmail.com>
2021-05-10 19:09:59 +0800

    revamped css and added javascript

---
+++ my_profile/script.js
@@ -0,0 +1,37 @@
+const body = document.getElementById('hobbies')
+const slides = document.querySelectorAll('.hobby')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+let activeSlide = 0
+
+const setBgToBody = () => {
+  body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
+    ${slides[activeSlide].style.backgroundImage}`
+}
+
+const removeActive = () => {
+  slides.forEach((slide) => slide.classList.remove('active'))
+}
+
+const addActive = () => {
+  slides[activeSlide].classList.add('active')
+}
+
+leftBtn.onclick = () => {
+  activeSlide--
+  if (activeSlide < 0) activeSlide = slides.length - 1
+  removeActive()
+  addActive()
+  setBgToBody()
+}
+
+rightBtn.onclick = () => {
+  activeSlide++
+  if (activeSlide > slides.length - 1) activeSlide = 0
+  removeActive()
+  addActive()
+  setBgToBody()
+}
+
+setBgToBody()
